2831a3a9f8d4c11e474af0b7805bf854239804b2
alerario <alerario@gmail.com>
2016-10-05 12:56:37 -0300

    Baseline - exclusão com mensagem de confirmação Inicio do desenvolvimento de busca seletiva listselect.xhtml

--- src/TeCService/target/generated-sources/annotations/br/data/entity/Teste_.java
+++ src/TeCService/target/generated-sources/annotations/br/data/entity/Teste_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-04T22:30:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-05T12:54:11")
 @StaticMetamodel(Teste.class)
 public class Teste_ { 
 
