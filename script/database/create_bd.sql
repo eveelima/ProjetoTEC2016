5c6df4e96fb3ecdb430b0b150c3e045919a92876
Alexandre L'Erario <alerario@gmail.com>
2016-10-09 10:49:18 -0300

    correcao do script para criacao do banco de dados

--- script/database/create_bd.sql
+++ script/database/create_bd.sql
@@ -4,8 +4,8 @@ CREATE DATABASE tecservice
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'pt_BR.UTF-8'
-    LC_CTYPE = 'pt_BR.UTF-8'
+--    LC_COLLATE = 'pt_BR.UTF-8'
+--    LC_CTYPE = 'pt_BR.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
